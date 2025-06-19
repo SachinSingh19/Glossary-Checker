9a265ce1c63293303bec9b6483e57c81ad95b92a
SACHIN SINGH <sacbai@gmail.com>
2025-06-19 12:13:37 +0200

    Update app8a.py

--- app8a.py
+++ app8a.py
@@ -1,6 +1,6 @@
-streamlit
-pandas 
-PyPDF2
+import streamlit as st
+import pandas as pd
+import PyPDF2
 from collections import Counter
 
 st.set_page_config(page_title="PDF Glossary Checker", layout="centered")
